202092e114e120a2e35c0c719ac6b830eff2662b
Lithara Perera <litharaperera2002@gmail.com>
2024-08-02 23:41:58 +0530

    refactor: Tailwind.config.ts

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MeetingTypeList  = () => {
+  return (
+    <section className='grid frid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+        <div className='bg-orange-1'>
+
+        </div>
+    </section>
+  )
+}
+
+export default MeetingTypeList;
