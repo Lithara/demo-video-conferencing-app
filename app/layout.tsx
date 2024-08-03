bdd402e04fca8d459e1dde7d3b05da70582d5545
Lithara Perera <litharaperera2002@gmail.com>
2024-08-04 00:44:14 +0530

    refactor: Complete project

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "DEMO",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
@@ -26,7 +26,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/demo-logo.svg",
           },
           variables: {
             colorText: "#fff",
